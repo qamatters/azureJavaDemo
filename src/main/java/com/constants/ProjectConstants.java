ddfcd0c6d7115de1420c12fe1958d285d4bb78fd
qamatters <mathpal.deepak1@gmail.com>
2022-03-17 17:09:49 +0530

    constants added

---
+++ src/main/java/com/constants/ProjectConstants.java
@@ -0,0 +1,11 @@
+package com.constants;
+
+public class ProjectConstants {
+    public static final String ACCOUNT_NAME = "javacodevalidationdeepak";
+    public static final String DEFAULT_PROTOCOL =  "https";
+    public static final String END_POINT_SUFFIX = "core.windows.net";
+    public static final String CONTAINER_NAME = "deepakcontainer";
+    public static final String DB_NAME = "testdatabase";
+    public static final String SERVER_NAME = "testdatabaseserver88";
+    public static final String DB_USER_NAME = "admindeepak";
+}
